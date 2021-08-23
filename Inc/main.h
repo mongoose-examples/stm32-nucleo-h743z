03c1e9893704c084d243685e1e026a0de5dbcedd
Alex Alashkin <alex.gremlin@outlook.com>
2021-08-23 12:41:03 +0300

    Add example

---
+++ Inc/main.h
@@ -0,0 +1,64 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+#include "stm32h7xx_nucleo.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* define USE_DHCP to enable DHCP, if disabled static address is used */
+
+/*Static IP ADDRESS*/
+#define IP_ADDR0   192
+#define IP_ADDR1   168
+#define IP_ADDR2   0
+#define IP_ADDR3   1
+
+/*NETMASK*/
+#define NETMASK_ADDR0   255
+#define NETMASK_ADDR1   255
+#define NETMASK_ADDR2   255
+#define NETMASK_ADDR3   0
+
+/*Gateway Address*/
+#define GW_ADDR0   192
+#define GW_ADDR1   168
+#define GW_ADDR2   0
+#define GW_ADDR3   1
+
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Makefile
@@ -0,0 +1,49 @@
+include generated_sources.mk
+
+PROG = firmware
+
+MONGOOSE_FLAGS = -DMG_ARCH=MG_ARCH_FREERTOS_LWIP
+MCU_FLAGS = -mcpu=cortex-m7 --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -DSTM32H743xx -DUSE_STM32H7XX_NUCLEO_144_MB1364 -DUSE_HAL_DRIVER -ffunction-sections -fstack-usage
+
+PROJECT_ROOT_PATH = $(realpath $(CURDIR)/../..)
+DOCKER ?= docker run --rm -v $(PROJECT_ROOT_PATH):$(PROJECT_ROOT_PATH) -w $(CURDIR) mdashnet/armgcc
+
+INCLUDES =	-I./Inc -I./Src 
+INCLUDES += -I./Middlewares/Third_Party/Mongoose 
+INCLUDES += $(DRIVERS_INCLUDES) $(FREERTOS_INCLUDES) $(LWIP_INCLUDES) 
+
+SOURCES = 	$(wildcard Src/*.c) Middlewares/Third_Party/Mongoose/mongoose.c
+SOURCES += 	$(BSP_SOURCES) $(HAL_SOURCES) $(FREERTOS_SOURCES) $(LWIP_SOURCES) $(OTHER_GENERATED_SOURCES)
+
+OBJECTS = $(SOURCES:%.c=obj/%.o) obj/startup_stm32h743xx.o 
+
+CFLAGS = -std=gnu11 -g3 -Os -W -Wall $(MCU_FLAGS) $(MONGOOSE_FLAGS) $(INCLUDES)
+
+# STMCube generated code produces a lot of "unused parameter" warnings
+CFLAGS += -Wno-unused-parameter
+
+LINKFLAGS =	-mcpu=cortex-m7 -T"SW4STM32/STM32H743ZI_Nucleo/STM32H743ZITx_FLASH.ld" --specs=nosys.specs \
+						-Wl,--gc-sections -static --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -u _printf_float -Wl,--start-group -lc -lm -Wl,--end-group
+
+build: $(PROG).bin
+
+$(PROG).bin: $(PROG).elf
+	$(DOCKER) arm-none-eabi-objcopy -O binary $< $@
+
+$(PROG).elf: $(OBJECTS)
+	$(DOCKER) arm-none-eabi-gcc $(OBJECTS) $(LINKFLAGS) -o $@
+	$(DOCKER) arm-none-eabi-size $@
+
+obj/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(DOCKER) arm-none-eabi-gcc $(CFLAGS) -c $< -o $@
+
+obj/startup_stm32h743xx.o: SW4STM32/startup_stm32h743xx.s
+	@mkdir -p $(dir $@)
+	$(DOCKER) arm-none-eabi-gcc $(CFLAGS) -x assembler-with-cpp -c $< -o $@
+
+flash: $(PROG).bin
+	st-flash --reset write $< 0x8000000
+	
+clean:
+	@rm -rf $(PROG).* obj
